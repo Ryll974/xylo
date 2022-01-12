5e3111472e85ad25d32c4b2843ae62c1dfdf9bdc
Ryll974 <jullien.patrick@gmail.com>
2022-01-12 12:46:45 +0400

    commit 3 -- mise en forme des containers (arrondis et marges)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 
 void main() {
   runApp(
@@ -38,64 +39,71 @@ class _XyloPhoneState extends State<XyloPhone> {
         Expanded(
           child: TextButton(
               child: Container(
-                color: Colors.white,
+                margin: const EdgeInsets.fromLTRB(0, 2, 0, 2),
+                decoration: new BoxDecoration(color: Colors.white,borderRadius: BorderRadius.all(Radius.circular(12))),
               ),
               onPressed: () {
-                print('Pressed');
+                print('Pressed note 1');
               }),
         ),
         Expanded(
           child: TextButton(
               child: Container(
-                color: Colors.red,
+                margin: const EdgeInsets.fromLTRB(0, 2, 0, 2),
+                decoration: new BoxDecoration(color: Colors.red,borderRadius: BorderRadius.all(Radius.circular(12))),
               ),
               onPressed: () {
-                print('Pressed');
+                print('Pressed note 2');
               }),
         ),
         Expanded(
           child: TextButton(
               child: Container(
-                color: Colors.blue,
+                margin: const EdgeInsets.fromLTRB(0, 2, 0, 2),
+                decoration: new BoxDecoration(color: Colors.blue,borderRadius: BorderRadius.all(Radius.circular(12))),
               ),
               onPressed: () {
-                print('Pressed');
+                print('Pressed note 3');
               }),
         ),
         Expanded(
           child: TextButton(
               child: Container(
-                color: Colors.purple,
+                margin: const EdgeInsets.fromLTRB(0, 2, 0, 2),
+                decoration: new BoxDecoration(color: Colors.purple,borderRadius: BorderRadius.all(Radius.circular(12))),
               ),
               onPressed: () {
-                print('Pressed');
+                print('Pressed note 4');
               }),
         ),
         Expanded(
           child: TextButton(
               child: Container(
-                color: Colors.yellow,
+                margin: const EdgeInsets.fromLTRB(0, 2, 0, 2),
+                decoration: new BoxDecoration(color: Colors.yellow,borderRadius: BorderRadius.all(Radius.circular(12))),
               ),
               onPressed: () {
-                print('Pressed');
+                print('Pressed note 5');
               }),
         ),
         Expanded(
           child: TextButton(
               child: Container(
-                color: Colors.green,
+                margin: const EdgeInsets.fromLTRB(0, 2, 0, 2),
+                decoration: new BoxDecoration(color: Colors.green,borderRadius: BorderRadius.all(Radius.circular(12))),
               ),
               onPressed: () {
-                print('Pressed');
+                print('Pressed note 6');
               }),
         ),
         Expanded(
           child: TextButton(
               child: Container(
-                color: Colors.grey,
+                margin: const EdgeInsets.fromLTRB(0, 2, 0, 2),
+                decoration: new BoxDecoration(color: Colors.grey,borderRadius: BorderRadius.all(Radius.circular(12))),
               ),
               onPressed: () {
-                print('Pressed');
+                print('Pressed note 7');
               }),
         ),
       ],
